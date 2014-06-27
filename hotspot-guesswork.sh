72d2e8b2a815f951e21d0d29d4e92c84fe852384
Enthymeme <luvhummel@gmail.com>
2014-06-27 19:50:09 -0300

    undo some of the fuckery

--- hotspot-guesswork.sh
+++ hotspot-guesswork.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. sh-functions
 LC_NUMERIC=C
 
 while [ "$1" ]; do
@@ -8,12 +9,12 @@ while [ "$1" ]; do
 		sizes=${1#*=}
 		shift
 		;;
-	base_size_line=*)
-		base_size_line=${1#*=}
-		shift
-		;;
 	replace=*)
 		replace=${1#*=}
+		shift
+		;;
+	base_size_line=*)
+		base_size_line=${1#*=}
 		shift
 		;;
 	dry_run=*)
@@ -28,36 +29,50 @@ while [ "$1" ]; do
 done
 
 : ${c:?no file specified}
+target=${c%.*}
+orientation=${c#*.}
+c=$target
+: ${target:?undefined}
 : ${sizes:?no size specified}
 : ${replace:=1}
 : ${dry_run:=0}
 
-[ -z "$base_size_line" ] && base_size_line=$(< config/32/$c)
-: ${base_size_line:?undefined}
+filename="$(basename $target).png"
+target="${c}.in"
+if [ "$orientation" = "left" ]; then
+	target="${c}.in_left"
+	c="${c}.left"
+	filename="${filename/.png/_left.png}"
+fi
+: ${filename:?undefined}
+nominal_size=$(cut -d' ' -f1 <<< $base_size_line)
+: ${nominal_size:?undefined}
+dynaspot ${nominal_size}/${filename}
+base_hotspot_x=$(cut -d' ' -f2 <<< $base_size_line)
+: ${base_hotspot_x:undefined}
+base_hotspot_y=$(cut -d' ' -f3 <<< $base_size_line)
+: ${base_hotspot_y:undefined}
+
+base_size_x=$size_x
+base_size_y=$size_y
+echo "$nominal_size ${base_hotspot_x} ${base_hotspot_y} $filename"
 
 for s in $sizes; do
-	size_int=${s%x*}
-	config_file=config/${s}/${c}
+	config_file=config/${s}/${target}
 	if [ -e $config_file -a "$replace" = "0" ]; then
 		echo "$config_file: skipping $s" >&2
 		continue
 	fi
-	base_size=$(echo $base_size_line|cut -d' ' -f1)
-	: ${base_size:?undefined}
-	hotspots=($(echo "$base_size_line"|cut -d' ' -f2,3))
-	: ${hotspots:?undefined}
-	filename=$(echo $base_size_line|cut -d' ' -f4)
-	: ${filename:?undefined}
-	[ "$debug_print" ] || echo "using $base_size ${hotspots[@]} $filename as base size"
-	debug_print=1
-	for n in ${hotspots[@]}; do
-		result=$(echo -e scale=2\\n$n \* \($size_int / $base_size\)|bc)
-		result=$(printf '%.0f' $result)
-		new_hotspots="$new_hotspots $result"
-	done
-	: ${new_hotspots:?undefined}
-	newsize="${size_int}${new_hotspots} ${s}/$(basename $filename)"
-	unset new_hotspots
-	echo "$config_file: $newsize"
-	[ "$dry_run" = "1" ] || echo "${newsize}" > $config_file
+	png="${s}/${filename}"
+	if [ ! -e $png ]; then
+		./make-pngs.sh sizes=${s} target=$c || exit 1
+	fi
+	dynaspot $png
+	echo "$png: ${size_x}x${size_y}"
+	hotspot_x=$(calc "$base_hotspot_x * ($size_x / $base_size_x)")
+	hotspot_y=$(calc "$base_hotspot_y * ($size_y / $base_size_y)")
+
+	output="${s} ${hotspot_x} ${hotspot_y} ${s}/$(basename $filename)"
+	echo "$config_file: $output"
+	[ "$dry_run" = "1" ] || echo "${output}" > $config_file
 done
