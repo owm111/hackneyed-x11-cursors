0298fe4326e0ba12c4e027c8e706ddd9fa6f858c
Enthymeme <luvhummel@gmail.com>
2014-06-27 21:54:14 -0300

    boom!

---
+++ deps.mk
@@ -0,0 +1,27 @@
+all-scroll: svg/all-scroll.svg
+center_ptr: svg/center_ptr.svg
+closedhand: svg/closedhand.svg
+color-picker color-picker.left: svg/color-picker.svg
+crosshair: svg/crosshair.svg
+default default.left: svg/default.svg
+draft draft.left: svg/draft.svg
+e-resize: svg/e-resize.svg
+ew-resize: svg/ew-resize.svg
+move: svg/move.svg
+ne-resize: svg/ne-resize.svg
+not-allowed: svg/not-allowed.svg
+nwse-resize: svg/nwse-resize.svg
+openhand: svg/openhand.svg
+pencil: svg/pencil.svg
+pirate: svg/pirate.svg
+plus: svg/plus.svg
+pointer: svg/pointer.svg
+row-resize: svg/row-resize.svg
+split_v: svg/split_v.svg
+tcross: svg/tcross.svg
+text: svg/text.svg
+up_arrow: svg/up_arrow.svg
+wayland-cursor-srsbsnss: svg/wayland-cursor-srsbsnss.svg
+wayland-cursor: svg/wayland-cursor.svg
+X_cursor: svg/X_cursor.svg
+zoom: svg/zoom.svg
