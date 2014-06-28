d22eaa52f0d0b08c7d8956076c725bc3ff4badd7
Enthymeme <enthymeme@zoho.com>
2014-06-27 21:54:14 -0300

    boom!

---
+++ overlay-deps.mk
@@ -0,0 +1,7 @@
+alias alias.left: svg/default.svg svg/overlay-curved-arrow.svg
+copy copy.left: svg/default.svg svg/overlay-plus-sign.svg
+progress progress.left: svg/default.svg svg/wait-static
+help help.left: svg/default.svg svg/overlay-question-arrow.svg
+context-menu context-menu.left: svg/default.svg svg/overlay-menu.svg
+no-drop no-drop.left: svg/default.svg svg/not-allowed.svg
+dnd-move dnd-move.left: svg/default.svg svg/overlay-fleur.svg
