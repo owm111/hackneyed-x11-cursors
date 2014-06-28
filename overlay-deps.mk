0e9c48c6787d7402b610ef4e2baa4bd8379b6be9
Enthymeme <luvhummel@gmail.com>
2014-06-28 16:20:20 -0300

    forgot ext

--- overlay-deps.mk
+++ overlay-deps.mk
@@ -1,6 +1,6 @@
 alias alias.left: svg/default.svg svg/overlay-curved-arrow.svg
 copy copy.left: svg/default.svg svg/overlay-plus-sign.svg
-progress progress.left: svg/default.svg svg/wait-static
+progress progress.left: svg/default.svg svg/wait-static.svg
 help help.left: svg/default.svg svg/overlay-question-arrow.svg
 context-menu context-menu.left: svg/default.svg svg/overlay-menu.svg
 no-drop no-drop.left: svg/default.svg svg/not-allowed.svg
