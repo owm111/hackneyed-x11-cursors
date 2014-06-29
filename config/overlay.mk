ee8a5d7fd655e53418844ca0ccfa0e9a54663f4f
Enthymeme <luvhummel@gmail.com>
2014-06-29 00:15:53 -0300

    add missing deps; change that of copy

--- config/overlay.mk
+++ config/overlay.mk
@@ -1,7 +1,9 @@
 alias alias.left: svg/default.svg svg/overlay-curved-arrow.svg config/overlay/alias*
-copy copy.left: svg/default.svg svg/overlay-plus-sign.svg config/overlay/copy*
+copy copy.left: svg/default.svg svg/overlay-plus-sign-shadowed.svg config/overlay/copy*
 progress progress.left: svg/default.svg svg/wait-static.svg config/overlay/progress*
 help help.left: svg/default.svg svg/overlay-question-mark.svg config/overlay/help*
 context-menu context-menu.left: svg/default.svg svg/overlay-menu.svg config/overlay/context-menu*
 no-drop no-drop.left: svg/default.svg svg/not-allowed.svg config/overlay/no-drop*
 dnd-move dnd-move.left: svg/default.svg svg/overlay-fleur.svg config/overlay/dnd-move*
+zoom-in zoom-in.left: svg/zoom.svg config/overlay/zoom-in*
+zoom-out zoom-out.left: svg/zoom.svg config/overlay/zoom-out*
