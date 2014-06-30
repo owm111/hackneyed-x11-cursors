f7dac5e4c5c3db9c0325508e7d99d61f783e4af5
Enthymeme <enthymeme@zoho.com>
2014-06-30 15:56:46 -0300

    wait a minute

--- make-config.sh
+++ make-config.sh
@@ -3,36 +3,13 @@
 . sh-functions
 parse_cmdline "$@"
 
-: ${orientation:=in}
 dest=${target}.${orientation}
 
 rm -f $dest
-case $orientation in
-	*left)
-		overlay_name=${target}_left ;;
-	*)
-		overlay_name=${target} ;;
-esac
-
 for s in $sizes; do
 	if ! [ -e config/$s ]; then
 		echo "size $s not found"
 		exit 1
 	fi
-	source_file=config/${s}/${dest}
-	if ! [ -e $source_file -a -e config/overlay/${overlay_name} ]; then
-		replace_with=${overlay_name}.png
-		echo "$source_file was not found, using basic target from overlay"
-		source config/overlay/${overlay_name} || exit 1
-		: ${svg:?undefined}
-		source_file="config/${s}/$(basename $svg .svg).${orientation}"
-		png_name=$(basename $(cut -d' ' -f4 $source_file))
-		: ${png_name:?undefined}
-	fi
-	if [ "$replace_with" ]; then
-		sed "s/$png_name/$replace_with/g" $source_file >> $dest
-	else
-		cat $source_file >> $dest || exit 1
-	fi
-	unset replace_with
+	cat config/${s}/${dest} >> $dest || exit 1
 done
