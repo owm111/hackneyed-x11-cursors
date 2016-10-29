ab010724823d3dbb86b553893aeb247318641097
Richard <luvhummel@gmail.com>
2016-10-29 18:16:32 -0200

    update deps

--- deps.mk
+++ deps.mk
@@ -1,6 +1,6 @@
 all-scroll: svg/all-scroll.svg
 center_ptr: svg/center_ptr.svg
-closedhand: svg/closedhand.svg
+closedhand closedhand.left: svg/closedhand.svg
 color-picker color-picker.left: svg/color-picker.svg
 crosshair: svg/crosshair.svg
 default default.left: svg/default.svg
@@ -11,30 +11,26 @@ move: svg/move.svg
 ne-resize: svg/ne-resize.svg
 not-allowed: svg/not-allowed.svg
 nwse-resize: svg/nwse-resize.svg
-openhand: svg/openhand.svg
+openhand openhand.left: svg/openhand.svg
+pencil pencil.left: svg/pencil.svg
 pencil: svg/pencil.svg
-pencil.left: svg/pencil.svg
 pirate: svg/pirate.svg
 plus: svg/plus.svg
-pointer: svg/pointer.svg
+pointer pointer.left: svg/pointer.svg
 row-resize: svg/row-resize.svg
 split_v: svg/split_v.svg
 tcross: svg/tcross.svg
 text: svg/text.svg
 up_arrow: svg/up_arrow.svg
-wayland-cursor-srsbsnss: svg/wayland-cursor-srsbsnss.svg
 wayland-cursor: svg/wayland-cursor.svg
 X_cursor: svg/X_cursor.svg
-zoom: svg/zoom.svg
-zoom.left: svg/zoom.svg
-zoom-in: zoom config/overlay/zoom-in svg/overlay-plus-sign.svg
-zoom-out: zoom config/overlay/zoom-out svg/overlay-minus-sign.svg
-zoom-in.left: zoom.left config/overlay/zoom-in_left svg/overlay-plus-sign.svg
-zoom-out.left: zoom.left config/overlay/zoom-out_left svg/overlay-minus-sign.svg
+zoom zoom.left: svg/zoom.svg
+zoom-in zoom-in.left: zoom config/overlay/zoom-in svg/overlay-plus-sign.svg
+zoom-out zoom-out.left: zoom config/overlay/zoom-out svg/overlay-minus-sign.svg
 based_arrow_down: svg/based_arrow_down.svg
 based_arrow_up: svg/based_arrow_up.svg
 top_tee: svg/top_tee.svg
 exchange: svg/exchange.svg
 draped_box: svg/draped_box.svg
 ul_angle: svg/ul_angle.svg
-vertical-text: svg/vertical-text.svg
+
