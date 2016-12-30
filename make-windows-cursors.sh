857a7181084df86ecfc06d8fc828693e3a445520
Richard <luvhummel@gmail.com>
2016-12-29 23:16:05 -0200

    massive rewriting

--- make-windows-cursors.sh
+++ make-windows-cursors.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-CURSORS="default default_big help help_big progress progress_big  
-wait text text_big crosshair crosshair_big pencil pencil_big
- not-allowed not-allowed_big ns-resize ns-resize_big ew-resize 
-ew-resize_big nesw-resize nesw-resize_big nwse-resize nwse-resize_big up_arrow
-up_arrow_big pointer pointer_big move move_big n-resize n-resize_big s-resize
-s-resize_big e-resize e-resize_big w-resize w-resize_big ne-resize ne-resize_big
-nw-resize nw-resize_big se-resize se-resize_big sw-resize sw-resize_big"
-LCURSORS="default default_big help help_big progress progress_big
-pencil pencil_big pointer pointer_big"
+CURSORS="default help progress wait text crosshair pencil not-allowed
+ns-resize ew-resize nesw-resize nwse-resize up_arrow pointer move
+n-resize s-resize e-resize w-resize ne-resize nw-resize se-resize sw-resize"
 
-rm -rf ico
-mkdir ico
+with_svg="src/prosthesis.svg"
 
-has_lcursor()
+mkdir ico || exit 1
+
+find_variants()
 {
+	leftlist="config/cur/left-handed"
+	largelist="config/cur/large"
+	
 	[ "$1" ] || exit 255
-	for c in $LCURSORS; do
-		[ "$c" = "$1" ] && return 0
-	done
-	return 1
+	
+	grep -Eq "(^|\W)$1($|\W)" $largelist && target_large=${1}_big
+	if grep -Eq "(^|\W)$1($|\W)" $leftlist; then
+		target_left=${1}_left
+		target_large_left=${1}_big_left
+	fi
 }
 
 get_hotspot()
@@ -34,26 +34,29 @@ get_hotspot()
 	hotspot_y=$3
 	: ${hotspot_x?undefined}
 	: ${hotspot_y?undefined}
-	echo "$cursor: $hotspot_x $hotspot_y"
+}
+
+generate_cur()
+{
+	target=$1
+	get_hotspot $target
+	dest_png="ico/${target}.png"
+	dest_ico="ico/${target}.ico"
+	echo "$target: $dest_ico (${hotspot_x},${hotspot_y})"
+	inkscape --without-gui -i $target -f $with_svg -d 96 -e $dest_png >/dev/null || exit 1
+	convert $dest_png $dest_ico || exit 1
+	util/ico2cur $dest_ico -x $hotspot_x -y $hotspot_y || exit 1
 }
 
 for c in $CURSORS; do
-	get_hotspot $c
-	dest_png="ico/${c}.png"
-	dest_ico="ico/${c}.ico"
-	echo "$c: $dest_ico"
-	inkscape --without-gui -i $c -f src/prosthesis.svg -d 96 -e $dest_png >/dev/null || exit 1
-	convert $dest_png $dest_ico || exit 1
-	python ico2cur.py $dest_ico -x $hotspot_x -y $hotspot_y || exit 1
-	if has_lcursor $c; then
-		leftc="${c}_left"
-		get_hotspot $leftc
-		dest_pngl="ico/${c}_left.png"
-		dest_icol="ico/${c}_left.ico"
-		echo "$leftc: $dest_icol"
-		inkscape --without-gui -i $leftc -f src/prosthesis.svg -d 96 -e $dest_pngl >/dev/null || exit 1
-		convert $dest_pngl $dest_icol || exit 1
-		python ico2cur.py $dest_icol -x $hotspot_x -y $hotspot_y || exit 1
-	fi
+	generate_cur $c
+	find_variants $c
+	[ "$target_large" ] && generate_cur $target_large
+	[ "$target_left" ] && generate_cur $target_left
+	[ "$target_large_left" ] && generate_cur $target_large_left
+	unset target_large target_left target_large_left
 done
-rm -f ico/*.{png,ico}
+mkdir -p Hackneyed-Windows/{Standard,King-size} || exit 1
+mv ico/*_big*.cur Hackneyed-Windows/King-size || exit 1
+mv ico/*.cur Hackneyed-Windows/Standard || exit 1
+7z a Hackneyed-Windows.7z Hackneyed-Windows || exit 1
