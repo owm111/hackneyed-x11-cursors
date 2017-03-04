e88d3870475f99ff5cc9b8bed67aad32f4519ff0
Richard <luvhummel@gmail.com>
2017-03-04 19:13:06 -0300

    remove stale code

--- make-config.sh
+++ make-config.sh
@@ -28,43 +28,15 @@
 . sh-functions
 parse_cmdline "$@"
 
-case $orientation in
-*left)
-	dest=${target}.in_left
-	overlay_name=${target}
-	orientation=in_left
-	;;
-in|*)
-	dest=${target}.in
-	overlay_name=${target}
-	orientation=in
-	;;
-esac
+dest=${target}.in
+
+[ "$orientation" = "left" ] && dest=${target}.in_left
+
 rm -f $dest
 
 for s in $sizes; do
-	if ! [ -e config/$s ]; then
-		echo "size $s not found"
-		exit 1
-	fi
+	[ -e config/$s ] || die "size $s not found"
 	source_file=config/${s}/${dest}
-	if ! [ -e $source_file ]; then
-		if [ -e config/overlay/${overlay_name} ]; then
-			replace_with=${overlay_name}.png
-			echo "$source_file was not found, using basic target from overlay"
-			source config/overlay/${overlay_name} || exit 1
-			: ${svg:?undefined}
-			source_file="config/${s}/$(basename $svg .svg).${orientation}"
-			png_name=$(basename $(cut -d' ' -f4 $source_file))
-			: ${png_name:?undefined}
-		else
-			die "No source file was found"
-		fi
-	fi
-	if [ "$replace_with" ]; then
-		sed "s/$png_name/$replace_with/g" $source_file >> $dest
-	else
-		cat $source_file >> $dest || exit 1
-	fi
-	unset replace_with
+	[ -e $source_file ] || die "No source file found for size $s"
+	cat $source_file >> $dest || exit 1
 done
