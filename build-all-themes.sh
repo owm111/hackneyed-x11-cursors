867d88f1c49d46eed5f02176676ff5f5c83248fe
Richard <luvhummel@outlook.com>
2019-11-09 17:33:18 -0300

    call make with -O

--- build-all-themes.sh
+++ build-all-themes.sh
@@ -2,5 +2,5 @@
 nprocs=$(getconf _NPROCESSORS_ONLN)
 : ${nprocs:=4}
 nprocs=$((nprocs+1))
-make clean && make -j${nprocs} all-dist preview windows-cursors && \
-make clean && make COMMON_SOURCE=theme/common-dark.svg RSVG_SOURCE=theme/right-handed-dark.svg LSVG_SOURCE=theme/left-handed-dark.svg THEME_NAME=Hackneyed-Dark -j${nprocs} all-dist preview windows-cursors
+make clean && make -O -j${nprocs} all-dist preview windows-cursors && \
+make clean && make -O -j${nprocs} COMMON_SOURCE=theme/common-dark.svg RSVG_SOURCE=theme/right-handed-dark.svg LSVG_SOURCE=theme/left-handed-dark.svg THEME_NAME=Hackneyed-Dark all-dist preview windows-cursors
