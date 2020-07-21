63ae7aafc443b559ab46ab47480a9606121695cd
Richard <luvhummel@outlook.com>
2020-07-21 15:17:53 -0300

    make clean in the end [does it even matter?]

--- build-all-themes.sh
+++ build-all-themes.sh
@@ -4,3 +4,4 @@ nprocs=$(getconf _NPROCESSORS_ONLN)
 nprocs=$((nprocs+1))
 make clean && make -O -j${nprocs} all-dist preview windows-cursors && \
 make clean && make -O -j${nprocs} COMMON_SOURCE=theme/common-dark.svg RSVG_SOURCE=theme/right-handed-dark.svg LSVG_SOURCE=theme/left-handed-dark.svg THEME_NAME=Hackneyed-Dark all-dist preview windows-cursors
+make clean
